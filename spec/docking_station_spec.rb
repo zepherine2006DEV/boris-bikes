e8a5ba5e0264ff66c6161bd6b946379347d1120b
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-05-31 17:34:46 +0100

    Ex.10 Making Docking stations realease bikes"
    exit
    q

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it 'releases a bike' do
     expect(DockingStation.new).to respond_to(:release_bike)
+  end
+  it 'checks if bike is working' do
+    bike = subject.release_bike
+    expect(bike.working?).to equal(true)
   end
 end
